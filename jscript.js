ed0ae51c40e0d56eeff00654b93a9deb8bf523f0
gabrielpieptea <gabriel.pieptea@gmail.com>
2020-01-18 19:42:10 +0200

    first version - unfinished

--- jscript.js
+++ jscript.js
@@ -1,5 +1,6 @@
+
 const container = document.getElementById('container');
-const containerCopy=document.getElementById('container');
+let n = 12;
 
 //grid
 function makeRows(rows, cols){
@@ -11,17 +12,41 @@ function makeRows(rows, cols){
       event.target.style.backgroundColor = 'red';
     });
     container.appendChild(cell).className = "grid-item";
+  }
+}
+makeRows(n,n);
 
+//clear container function
+  function clearBox(elementID){
+    document.getElementById(elementID).innerHTML = '';
+    n = prompt("Enter n: ");
+    if(isNaN(n)){
+      alert('Please enter a number');
+      n = prompt("Enter n: ");
+
+    }
+    makeRows(n,n);
   }
 
-}
-
-makeRows(10,10);
-
-
-//button
+//reset button
 const gridButton = document.createElement('button');
-gridButton.innerText = 'TEST ME';
-gridButton.addEventListener('click', e => new makeRows(10,10));
+gridButton.innerText = 'RESET GRID';
+gridButton.addEventListener('click', e => clearBox('container'));
 gridButton.setAttribute("id", "gridButton");
 document.body.appendChild(gridButton);
+
+//not done
+//rgb gridButton
+const rgbButton = document.createElement('button');
+rgbButton.innerText = 'Random Color Generator';
+//rgbButton.addEventListener('click', e => chooseColor());
+rgbButton.setAttribute('id', 'gridButton');
+//document.body.appendChild(rgbButton);
+
+//random rgb function
+function rgbcolor(){
+  var o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+// test area
